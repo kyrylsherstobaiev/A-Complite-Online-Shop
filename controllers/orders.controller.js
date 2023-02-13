5b54540c749403db8367aa6f062d6a4d434826f8
kyrylsherstobaiev <novemberua@gmail.com>
2023-02-13 16:22:53 +0200

    A_complete_online_shop_with_System_Of_Payment_System_STRIBE

---
+++ controllers/orders.controller.js
@@ -0,0 +1,73 @@
+const stripe = require("stripe")('sk_test_51MazN7JXwWzohOszwgXFPYAHOxOU7rtoJmFSirXb8t954qxjw8TaxcJ7Qe2lkzEVX4QcwafyxZPYkfVkjKum8tFw00m7IlAqSC');
+const Order = require('../models/order.model');
+const User = require('../models/user.model');
+
+async function getOrders(req, res, next) {
+    try {
+        const orders = await Order.findAllForUser(res.locals.uid);
+        res.render('customer/orders/all-orders', {
+            orders: orders,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function addOrder(req, res, next) {
+    const cart = res.locals.cart;
+
+    let userDocument;
+    try {
+        userDocument = await User.findById(res.locals.uid);
+        await cart.updatePrices();
+    } catch (error) {
+        return next(error);
+    }
+
+    const order = new Order(cart, userDocument);
+
+    try {
+        await order.save();
+    } catch (error) {
+        next(error);
+        return;
+    }
+
+    req.session.cart = null;
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: cart.items.map(function (item) {
+            return {
+                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                price_data: {
+                    currency: 'usd',
+                    product_data: {
+                        name: item.product.title
+                    },
+                    unit_amount: +item.product.price.toFixed(2) * 100
+                },
+                quantity: item.quantity
+            }
+        }),
+        mode: 'payment',
+        success_url: `http://localhost:3000/orders/success`,
+        cancel_url: `http://localhost:3000/orders/failure`,
+    });
+
+    res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+    res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+    res.render('customer/orders/failure');
+}
+
+module.exports = {
+    addOrder: addOrder,
+    getOrders: getOrders,
+    getSuccess: getSuccess,
+    getFailure: getFailure
+};
